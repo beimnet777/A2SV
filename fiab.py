d5b16e3308441726184401d2624bc89144b3e92f
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-04-09 00:55:39 -0700

    Create fiab.py

---
+++ fiab.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        a=0
+        b=1
+        if n==0:
+            return 0
+        for i in range(n-1):
+            c=a+b
+            a=b
+            b=c
+        return b
+        
+        
