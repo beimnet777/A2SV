17d505c9152a069500db3c05e1fc55602d73214a
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-04-09 00:45:53 -0700

    Create power_of_four.py

---
+++ power_of_four.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n==1:
+            return True
+        elif n<1:
+            return False
+        else:
+            return self.isPowerOfFour(n/4)
