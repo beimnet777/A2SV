a6c7384643b9e18a31345d5751e729c28e4afaec
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-04-16 04:29:04 -0700

    Update max_number_of _kPairs.py

--- max_number_of _kPairs.py
+++ max_number_of _kPairs.py
@@ -1,8 +1,15 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        nums.sort()
+            nums.sort()
         i,j=0,len(nums)-1
         count=0
         while i<j:
             if nums[i]+nums[j]==k:
                 count+=1
+                i+=1
+                j-=1
+            elif nums[i]+nums[j]<k:
+                i+=1
+            else:
+                j-=1
+        return count
