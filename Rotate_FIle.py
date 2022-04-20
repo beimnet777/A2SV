7bd73d357c023795a44cec8b8b65446348111eff
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-04-20 08:56:21 -0700

    Create Rotate_FIle.py

---
+++ Rotate_FIle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        new
+        for i in range(k):
+            nums.insert(0,nums.pop())
+        
