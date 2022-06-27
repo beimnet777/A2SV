e8fea5e0b51c729dd4a4b75fadc6667d7ce585cb
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-06-26 23:51:58 -0700

    Create Kth_Largest_element.py

---
+++ Kth_Largest_element.py
@@ -0,0 +1,8 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums=[-i for i in nums]
+        heapq.heapify(nums)
+        for i in range(k-1):
+            heapq.heappop(nums)
+        return -heapq.heappop(nums)
