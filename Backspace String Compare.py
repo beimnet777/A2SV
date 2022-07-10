32e4fe3681c93e52f616fe5d4ccde924316d51d5
BEIMNET BEKELE GUTA <91521577+beimnet777@users.noreply.github.com>
2022-07-10 00:39:18 -0700

    Update Backspace String Compare.py

--- Backspace String Compare.py
+++ Backspace String Compare.py
@@ -1,11 +1,14 @@
 class Solution:
+    
     def backspaceCompare(self, s: str, t: str) -> bool:
         stack_s=[]
         stack_t=[]
+        
         for i in range(len(s)):
             if s[i]=='#':
                 if len(stack_s)>0:stack_s.pop()
             else:stack_s.append(s[i])
+                
         for i in range(len(t)):
             if t[i]=='#':
                 if len(stack_t)>0:stack_t.pop()
